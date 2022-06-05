c956203f7ca083967cc2e719fecc1077498aa008
lipssmycode <544450236@qq.com>
2022-06-05 20:06:28 +0800

    set uid and pid

--- start_bot.sh
+++ start_bot.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+export CURRENT_PUID=$(id -u)
+export CURRENT_PGID=$(id -g)
 mkdir -p ./transmission/data ./transmission/downloads ./transmission/watch ./app/config ./app/data
 docker-compose up -d
